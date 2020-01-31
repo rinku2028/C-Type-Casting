c60cf4ad487229dd8aa3dbf904793cc7bcd7c9f3
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-31 23:15:32 +0545

    Add files via upload

---
+++ stringtointconvert.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conversion from string to int
+            string x = "10";
+            string y = "10";
+            int sum = Convert.ToInt32(x) + Convert.ToInt32(y);
+            int mul = Convert.ToInt32(x) * Convert.ToInt32(y);
+            int div = Convert.ToInt32(x) / Convert.ToInt32(y);
+;           Console.WriteLine("Sum:"+ "\t"+sum.ToString());
+            Console.WriteLine("Multiple value:" + "\t" + mul.ToString());
+            Console.WriteLine("Divide value:" + "\t" + div.ToString());
+            Console.ReadLine();
+
+                
+        }
+    }
+}
