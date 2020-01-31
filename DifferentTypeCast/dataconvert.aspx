66d6162278a2ad876240757137b38193721ae2de
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-31 23:14:11 +0545

    Add files via upload

---
+++ DifferentTypeCast/dataconvert.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="dataconvert.aspx.cs" Inherits="conversion.dataconvert" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+             <tr>
+            <td>
+            <asp:Label ID="lblfirstnum" runat="server" Text="Enter First Number:"></asp:Label></td>
+            <td><asp:TextBox ID="txtfirstnum" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+              <td><asp:Label ID="lblsecondnum" runat="server" Text="Enter Second Number:"></asp:Label></td>
+              <td><asp:TextBox ID="txtsecondnum" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td><asp:Button ID="btnadd" runat="server" Text="ADD" OnClick="btnadd_Click" /><asp:Button ID="btnmul" runat="server" Text="MULTIPLY" OnClick="btnmul_Click" /></td>
+                <td><asp:Button ID="btndiv" runat="server" Text="DIVIDE" OnClick="btndiv_Click" /></td>
+                <td><asp:Label ID="lblmessage" runat="server" Text=""/></td>
+                
+            </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DifferentTypeCast/dataconvert.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace conversion
+{
+    public partial class dataconvert : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnadd_Click(object sender, EventArgs e)
+        {
+            if(!String.IsNullOrEmpty(txtfirstnum.Text)&& (!String.IsNullOrEmpty(txtsecondnum.Text)))
+            {
+                int x = Convert.ToInt32(txtfirstnum.Text);
+                int y = Convert.ToInt32(txtsecondnum.Text);
+                int sum = x + y;
+                lblmessage.Text="Sum of"+ "\t" + txtfirstnum.Text.ToString() + "\t" + "and" +"\t" + txtsecondnum.Text.ToString()+"\t" +":"+ sum.ToString();
+            }
+            else
+            {
+                lblmessage.Text = "Please enter values in the textboxes!";
+                //lblmessage.Text=System.Drawing.Color.Red;
+
+            }
+
+        }
+
+        protected void btnmul_Click(object sender, EventArgs e)
+        {
+            if(!String.IsNullOrEmpty(txtfirstnum.Text)&&(!String.IsNullOrEmpty(txtsecondnum.Text)))
+            {
+                int x = Convert.ToInt32(txtfirstnum.Text);
+                int y = Convert.ToInt32(txtsecondnum.Text);
+                int mul = x / y;
+                lblmessage.Text = "Multipliation of" + "\t" + txtfirstnum.Text.ToString() + "\t" + "and" + "\t" + txtsecondnum.Text.ToString() + "\t" + ":" + mul.ToString();
+
+
+            }
+            else
+            {
+                lblmessage.Text = "Please enter values in the textboxes!!";
+            }
+        }
+
+        protected void btndiv_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(txtfirstnum.Text);
+            int y = Convert.ToSByte(txtsecondnum.Text);
+            int div = x / y;
+            if(!String.IsNullOrEmpty(txtfirstnum.Text)&& (!String.IsNullOrEmpty(txtsecondnum.Text)))
+            {
+                lblmessage.Text = "Divide of" + "\t" + txtfirstnum.Text.ToString() + "\t"+ "and" +"\t"+ txtsecondnum.Text.ToString() + "\t" + ":" + div.ToString();
+            }
+            else
+            {
+                lblmessage.Text = "Please enter values in the textboxes!!";
+            }
+
+        }
+    }
+}
