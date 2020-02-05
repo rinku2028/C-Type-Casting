f41149b80fad86a31924f94af821d7769d0743fd
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-05 18:35:37 +0545

    Add files via upload

---
+++ constkey.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp7
+{
+    class constkey
+
+    {
+        static void Main(string []args)
+        {
+            const double pi = 3.14159; //declaring constant
+            double rad;
+            Console.WriteLine("Please enter radius:");
+            rad = Convert.ToDouble(Console.ReadLine());
+            double areaofcircle = pi * rad * rad;
+            Console.WriteLine("Radius:{0},Area:{1}", rad, areaofcircle);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ converttodouble.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class converttodouble
+    {
+        static void Main(string[] args)
+        {
+            double x = 1899.33;
+            int i;
+
+            //cast double to int
+            i = (int)x;
+            Console.WriteLine(i);
+            Console.ReadLine(); //Output is 1899
+        }
+    }
+}
